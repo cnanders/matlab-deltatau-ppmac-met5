eddb08abb48a1d49d7810400a2df841a8e192907
Chris Anderson from MET5Workstation <cnanderson@lbl.gov>
2017-09-18 15:42:08 -0700

    updates that will never be used

--- tests/test_deltatau_ppmac.m
+++ tests/test_deltatau_ppmac.m
@@ -8,14 +8,18 @@ cDirVendor = fullfile(cDirSrc, 'vendor');
 % Add src
 addpath(genpath(fullfile(cDirSrc)))
 
+% Add javapath
+javaaddpath(fullfile(cDirVendor, 'ganymed-ssh2-build250', 'ganymed-ssh2-build250.jar'));
+
 % Add vendor ssh
 addpath(genpath(fullfile(cDirVendor, 'fileexchange', 'ssh2_v2_m1_r6')));
 
+% Add 
 cHost = '192.168.20.23'; % MET5 endstation subnet
 
 ppmac = deltatau.PowerPmac(...
     'cHost', cHost ...
 );
 
-ppmac.init();
+% ppmac.init();
 
